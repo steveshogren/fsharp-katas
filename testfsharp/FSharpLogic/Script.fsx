75f57bd6739a79ff78db8cb02f07526a95ea5291
Jack <steve.a.shogren@gmail.com>
2016-04-24 07:34:38 -0400

    Starting a class to test the LL adder

--- testfsharp/FSharpLogic/Script.fsx
+++ testfsharp/FSharpLogic/Script.fsx
@@ -2,14 +2,14 @@
   | Empty
   | Cons of 'a * LinkedList<'a>
 
-let l1 = Cons(1, Cons(2, Cons(3, Empty)))
+let l1 = Cons(1, Cons(2, Cons(3, Cons(4, Cons(5, Empty)))))
 
 let rec map (f:'a->'b) (l:LinkedList<'a>) : LinkedList<'b> = 
   match l with 
   | Cons(a, next) -> Cons(f a, map f next)
   | Empty -> Empty
 
-let rec filter (pred:'a->bool) (l:LinkedList<'a>) : LinkedList<'b> = 
+let rec filter (pred:'a->bool) (l:LinkedList<'a>) : LinkedList<'a> = 
   match l with 
   | Cons(a, next) -> if (pred a) then Cons(a, filter pred next) else filter pred next
   | Empty -> Empty
@@ -22,4 +22,15 @@ let rec fold (acc:'b -> 'a -> 'b) (init:'b) (l:LinkedList<'a>) : 'b =
 let toString l = fold (fun ret next -> ret + next.ToString()) "" l 
 
 System.Console.WriteLine(toString (map (fun a -> 1 + a) l1))
-System.Console.WriteLine(toString (filter (fun a -> a = 2) l1))
+System.Console.WriteLine(toString (filter (fun a -> a = 2 || a = 4) l1))
+
+type AddNumbers () = 
+    let l1 = Cons(2, Cons(4, Cons(3, Empty)))
+    let l2 = Cons(5, Cons(6, Cons(4, Empty)))
+
+    let add l1 l2 = l1
+    member t.runTests () = 
+        System.Console.WriteLine(add l1 l2 |> toString |> ((=) "708"))
+   
+AddNumbers().runTests()
+
