ee4740e7ae55b8988cf2a82f67f9a7460084195b
Jack <steve.a.shogren@gmail.com>
2016-04-30 12:15:21 -0400

    Finish the level order traversal

--- testfsharp/FSharpLogic/LevelTraversal.fs
+++ testfsharp/FSharpLogic/LevelTraversal.fs
@@ -40,43 +40,31 @@ module Tree =
                     iPrint ret rest next
         iPrint [] [node] []
 
-    let levelFold (f:'a->'b->'b) (init:'b) (node:TNode<'a>) =
-        let rec iPrint ret (current:TNode<'a> list) (next:TNode<'a> list) = 
-            match current, next with
-            | [], [] -> ret
-            | [], next -> iPrint ret next []
-            | node::rest, next -> 
-                let ret = f node.value ret
-                match node.left, node.right with
-                | Some l, Some r -> 
-                    iPrint ret rest (l::r::next)
-                | Some n, None 
-                | None, Some n -> 
-                    iPrint ret rest (n::next)
-                | None, None ->
-                    iPrint ret rest next
-        iPrint init [node] []
-
     let levelFoldOpt (f:'a option->'b->'b) (init:'b) (node:TNode<'a>) =
         let rec iPrint ret (current:(TNode<'a> option) list) (next:(TNode<'a> option) list) = 
+            Console.WriteLine("Cur: " + current.ToString());
+            Console.WriteLine("Next: " + next.ToString());
             match current, next with
             | [], [] -> ret
             | [], next -> iPrint ret next []
             | (Some node)::rest, next ->
+                Console.WriteLine("Some: " + node.value.ToString());
                 let ret = f (Some node.value) ret
-                iPrint ret rest (node.left::node.right::next)
+                Console.WriteLine(ret);
+                let next = List.append next [node.left;node.right]
+                iPrint ret rest next
             | None ::rest, next -> 
+                Console.WriteLine("None");
                 let ret = f None ret
+                Console.WriteLine(ret);
                 iPrint ret rest next
         iPrint init [Some node] []
 
-//    let printFold = levelFold (fun a b -> b + a.ToString()) ""
     let treeNodePrint = function
     | Some n -> n.ToString()
     | None -> "#"
 
-    let printFold = levelFoldOpt (fun a b -> b + (treeNodePrint a)) ""
-
+    let printFold = levelFoldOpt (fun next ret -> ret+(treeNodePrint next)) ""
 
 [<TestFixture>]
 type LevelTraversal () = 
@@ -86,5 +74,6 @@ type LevelTraversal () =
                          Some (TNode(20, Some(TNode(15, None, None)),
                                           Some(TNode(7,None,None)))))
 //        Assert.AreEqual(["3";"9";"20";"#";"#";"15";"7";"#";"#";"#";"#"], Tree.group(l1))
+        Assert.AreEqual(l1, (Tree.makeTree ["3";"9";"20";"#";"#";"15";"7";"#";"#";"#";"#"]))
         //Assert.AreEqual(["3";"9";"20";"#";"#";"15";"7";"#";"#";"#";"#"], Tree.group(l1))
         Assert.AreEqual("3920##157####", Tree.printFold(l1))
